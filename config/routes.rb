dbfdcf91aea28c9a805f566d3885baa0e8852d88
平間洸揮 <hiramakouki@MacBook-Pro.local>
2025-06-12 20:19:10 +0900

    レシピ登録機能の編集

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  root "posts#index"
+  root "recipes#index"
 end
