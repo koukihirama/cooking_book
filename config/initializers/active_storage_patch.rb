68805b6d064f6f9c949469109fc7936bf9472894
平間洸揮 <hiramakouki@MacBook-Pro.local>
2025-07-05 18:30:37 +0900

    エラー修正

--- config/initializers/active_storage_patch.rb
+++ config/initializers/active_storage_patch.rb
@@ -1,10 +1,12 @@
+require "active_storage/service/s3_service"
+
 Rails.application.config.to_prepare do
   ActiveStorage::Service::S3Service.class_eval do
-    private def upload_with_checksum(key, io, checksum, **options)
+    private def upload_with_disabled_checksum(key, io, checksum: nil, **options)
       upload_without_checksum(key, io, **options)
     end
 
     alias_method :upload_without_checksum, :upload
-    alias_method :upload, :upload_with_checksum
+    alias_method :upload, :upload_with_disabled_checksum
   end
 end
